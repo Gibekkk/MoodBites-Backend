31c87de3cd9db3e9fe56de2b0c5b68173198acb7
Gibekkk <www.gilbertwinardy@gmail.com>
2026-05-06 14:50:56 +0800

    Update MoodBitesApplicationTests.java

--- src/test/java/com/moodbites/restfulapi/MoodBitesApplicationTests.java
+++ src/test/java/com/moodbites/restfulapi/MoodBitesApplicationTests.java
@@ -2,12 +2,16 @@ package com.moodbites.restfulapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
 
 @SpringBootTest
+@MockBean(GoogleCredentials.class)
+@MockBean(FirebaseApp.class)
 class MoodBitesApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
-
 }
